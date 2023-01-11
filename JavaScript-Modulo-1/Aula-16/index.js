1a9c41421f87c1c8d7ae97debf8c6df08ce47a03
Vinicius.n.silva <vinicius.nascimentodasilva@outlook.com>
2023-01-11 07:06:39 -0300

    up

---
+++ JavaScript-Modulo-1/Aula-16/index.js
@@ -0,0 +1,17 @@
+
+/*
+const idade = prompt("Informe a sua idade:")
+
+if (idade > 18) {
+  alert("Você é maior de idade")
+} else {
+  alert("Você é menor de idade")
+
+  if (idade < 12) {
+    alert("Você é criança")
+  }
+}   */
+
+const resultado = (6 === 6) ? "Verdadeiro" : "False"
+
+console.log (resultado)
