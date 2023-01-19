5f64732cea11e2d9bef060d3b27c777ddeba55e4
Vinicius.n.silva <vinicius.nascimentodasilva@outlook.com>
2023-01-18 21:32:38 -0300

    up

---
+++ JavaScript-Modulo-1/Aula-19/switch.js
@@ -0,0 +1,16 @@
+const resultado = prompt("Escolha uma alternativa: \na)\nb)\nc)")
+
+
+
+switch (resultado) {
+  case "a":
+    alert("O resultado é 'a'")
+    break
+  case "b":
+    alert("O resultado é 'b'")
+    break
+  case "c":
+    alert("O resultado é 'c'")
+  default:
+    alert("Finalizando...")
+}
