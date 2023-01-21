4189e6fcfa225d3614b624a82aea4834205b2040
Vinicius.n.silva <vinicius.nascimentodasilva@outlook.com>
2023-01-21 11:54:40 -0300

    up

---
+++ JavaScript-Modulo-1/Exercicio-4/index.js
@@ -0,0 +1,12 @@
+const turista = prompt ("E aí, turista! Qual é o seu nome?")
+let cidades = ""
+let contagem = 0
+
+let continuar = prompt("Você visitou alguma cidade? (Sim/Não)")
+
+while (continuar === "Sim") {
+  let cidade = prompt("Qual é o nome da cidade visitada?")
+  // cidades = cidades 
+  cidades += 
+}
+  
