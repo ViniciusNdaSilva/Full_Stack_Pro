46d5e22716269f93e14d260443ca689209cabd6e
Vinicius.n.silva <vinicius.nascimentodasilva@outlook.com>
2023-01-21 09:44:19 -0300

    up

---
+++ JavaScript-Modulo-1/Aula-22/while.js
@@ -0,0 +1,14 @@
+let velocidade = 80 
+
+while (velocidade > 0) {
+  alert("O carro está a " + velocidade + " km/h")
+
+  velocidade -= 20 
+  alert("Diminuindo 20km/h")
+
+  if (velocidade === 40) {
+    break
+  }
+}
+
+alert("O carro parou")
