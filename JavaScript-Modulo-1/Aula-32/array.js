e37dba07f72fef644327a7a60c2fb1c8d16818bf
Vinicius.n.silva <vinicius.nascimentodasilva@outlook.com>
2023-03-27 22:05:08 -0300

    up

---
+++ JavaScript-Modulo-1/Aula-32/array.js
@@ -0,0 +1,8 @@
+const listaDeCompras = []
+
+listaDeCompras[0] = "Arroz"
+listaDeCompras[1] = "Feijão"
+listaDeCompras[2] = 7
+
+console.log(listaDeCompras)
+
---
+++ JavaScript-Modulo-1/Aula-33/array.js
@@ -0,0 +1,43 @@
+const arr = ["Frodo", "Sam", "Merry", "Pippin", "Gandalf", "Aragorn", "Legolas", "Gimli"]
+
+console.log(arr)
+
+// Adicionar Elementos 
+// push 
+arr.push("Boromir")
+console.log(arr)
+
+// unshift 
+arr.unshift("Boromir")
+console.log(arr)
+
+// Remover Elementos 
+// pop 
+arr.pop("Boromir")
+console.log(arr)
+
+// shift
+arr.shift("Boromir")
+console.log(arr)
+
+// includes: Verifica se um certo elemento está presente no array 
+const inclui = arr.includes("Gandalf")
+console.log(inclui)
+
+// indexOf: Encontra o índice que possui o elemento indicado, ou -1 caso ele não exista no array
+const indice = arr.indexOf("Gandalf")
+console.log(indice)
+
+// slice: Copia uma parte do array e devolve a parte copiada sem alterar o array original
+const hobbits = arr.slice(0, 4)
+// Também pode ser usado com números negativos referenciar o final do array
+const outros = arr.slice(-4)
+console.log(arr)
+console.log(hobbits)
+console.log(outros)
+
+// concat: Junta dois ou mais arrays e/ou itens e devolve o resultado (sem alterar nenhum dos arrays)
+const sociedade = hobbits.concat(outros, "Boromir")
+console.log(sociedade)
+console.log(hobbits)
+console.log(outros)
